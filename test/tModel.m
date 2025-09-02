8454fe3da82e235283edb8d36bbab943df2fedd5
Edu Benet Cerda <ebenetce@mathworks.com>
2025-09-02 11:45:31 +0200

    ignored files and else

---
+++ test/tModel.m
@@ -0,0 +1,16 @@
+classdef tModel < matlab.unittest.TestCase
+
+    methods (Test)
+
+        function tSWModel(tc)
+
+            prj = currentProject();
+            cd(prj.RootFolder)
+            dynare Smets_Wouters_2007_45.mod nolog
+            tc.verifyTrue(isfile('Smets_Wouters_2007_45/Output/Smets_Wouters_2007_45_results.mat'))
+            
+        end
+
+    end
+
+end
